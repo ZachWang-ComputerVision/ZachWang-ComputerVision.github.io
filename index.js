b52d5cd6e3f988a8c00b287768dbf720f251e92e
Zack Wang <2010zihao@gmail.com>
2023-02-14 18:37:31 -0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function Bio() {
   let bio = document.querySelector("#bioSection");
   let port = document.querySelector("#portSection");
+  let welcome = document.querySelector("#welcome");
   bio.style.display = "flex";
   port.style.display = "none";
+  welcome.style.color = "rgb(240, 255, 255)"
 };
 
 function Port() {
   let bio = document.querySelector("#bioSection");
   let port = document.querySelector("#portSection");
+  let welcome = document.querySelector("#welcome");
   bio.style.display = "none";
   port.style.display = "block";
+  welcome.style.color = "rgb(240, 255, 255)"
 };
